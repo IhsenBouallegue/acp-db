e469c2c7aea52de616c4a81064a17cdb451289fc
IhsenBouallegue <bouallegueihsen@gmail.com>
2024-11-21 14:12:22 +0100

    make dialogs always round

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { PageContainer } from "@/components/ui/page-container";
 import { PageHeading } from "@/components/ui/page-heading";
 import { PageSubheading } from "@/components/ui/page-subheading";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -49,7 +50,7 @@ export default function SettingsPage() {
   };
 
   return (
-    <div className="container">
+    <PageContainer>
       <PageHeading>Settings</PageHeading>
       <PageSubheading>Manage your account preferences and application settings</PageSubheading>
       <Card>
@@ -185,6 +186,6 @@ export default function SettingsPage() {
           </Button>
         </CardFooter>
       </Card>
-    </div>
+    </PageContainer>
   );
 }
